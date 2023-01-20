836f2dea1977ddb1c1351de1f5026b7aefc00264
Simon Xie <simon.xie@shopcanal.com>
2023-01-20 15:12:40 -0800

    numbers

--- intensifier/scripts/string_intensifier.py
+++ intensifier/scripts/string_intensifier.py
@@ -1,12 +1,12 @@
 import sys
-from string import ascii_lowercase
+from string import ascii_lowercase, digits
 
 string = sys.argv[1]
 
 new_word = ''
 for letter in string:
     letter_lowered = letter.lower()
-    if letter_lowered in ascii_lowercase:
+    if letter_lowered in ascii_lowercase or letter in  digits:
         new_word += f':{letter_lowered}-intensifies:'
     elif letter == ' ':
         new_word += f'   '
